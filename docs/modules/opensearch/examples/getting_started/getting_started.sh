4abaa11b04de50312959d41c1d4ea5506163d315
Siegfried Weber <mail@siegfriedweber.net>
2025-07-18 17:20:33 +0200

    Adapt the getting started guide

--- docs/modules/opensearch/examples/getting_started/getting_started.sh
+++ docs/modules/opensearch/examples/getting_started/getting_started.sh
@@ -10,8 +10,8 @@ set -euo pipefail
 # This script contains all the code snippets from the guide, as well as some assert tests
 # to test if the instructions in the guide work. The user *could* use it, but it is intended
 # for testing only.
-# The script will install the operators, create a superset instance and briefly open a port
-# forward and connect to the superset instance to make sure it is up and running.
+# The script will install the operators, create a OpenSearch instance and briefly open a port
+# forward and connect to the OpenSearch instance to make sure it is up and running.
 # No running processes are left behind (i.e. the port-forwarding is closed at the end)
 
 if [ $# -eq 0 ]
@@ -29,7 +29,7 @@ echo "Installing Operators with Helm"
 helm install --wait commons-operator oci://oci.stackable.tech/sdp-charts/commons-operator --version 0.0.0-dev
 helm install --wait secret-operator oci://oci.stackable.tech/sdp-charts/secret-operator --version 0.0.0-dev
 helm install --wait listener-operator oci://oci.stackable.tech/sdp-charts/listener-operator --version 0.0.0-dev
-helm install --wait superset-operator oci://oci.stackable.tech/sdp-charts/superset-operator --version 0.0.0-dev
+helm install --wait opensearch-operator oci://oci.stackable.tech/sdp-charts/opensearch-operator --version 0.0.0-dev
 # end::helm-install-operators[]
 ;;
 "stackablectl")
@@ -39,7 +39,7 @@ stackablectl operator install \
   commons=0.0.0-dev \
   secret=0.0.0-dev \
   listener=0.0.0-dev \
-  superset=0.0.0-dev
+  opensearch=0.0.0-dev
 # end::stackablectl-install-operators[]
 ;;
 *)
@@ -48,67 +48,65 @@ exit 1
 ;;
 esac
 
-echo "Installing bitnami PostgreSQL"
-# tag::install-bitnami-psql[]
-helm install superset oci://registry-1.docker.io/bitnamicharts/postgresql \
-    --version 16.5.0 \
-    --set auth.username=superset \
-    --set auth.password=superset \
-    --set auth.database=superset \
-    --wait
-# end::install-bitnami-psql[]
+echo "Creating OpenSearch security plugin configuration"
+# tag::apply-security-config[]
+kubectl apply -f opensearch-security-config.yaml
+# end::apply-security-config[]
 
-echo "Creating credentials secret"
-# tag::apply-superset-credentials[]
-kubectl apply -f superset-credentials.yaml
-# end::apply-superset-credentials[]
-
-echo "Creating Superset cluster"
-# tag::apply-superset-cluster[]
-kubectl apply -f superset.yaml
-# end::apply-superset-cluster[]
+echo "Creating OpenSearch cluster"
+# tag::apply-cluster[]
+kubectl apply -f opensearch.yaml
+# end::apply-cluster[]
 
 sleep 5
 
 for (( i=1; i<=15; i++ ))
 do
-  echo "Waiting for SupersetCluster to appear ..."
-  if eval kubectl get statefulset simple-superset-node-default; then
+  echo "Waiting for OpenSearchCluster to appear ..."
+  if eval kubectl get statefulset simple-opensearch-nodes-default; then
     break
   fi
 
   sleep 1
 done
 
-echo "Waiting on superset StatefulSet ..."
-# tag::wait-superset[]
-kubectl rollout status --watch statefulset/simple-superset-node-default --timeout 300s
-# end::wait-superset[]
+echo "Waiting on OpenSearch StatefulSet ..."
+# tag::await-cluster[]
+kubectl rollout status --watch statefulset/simple-opensearch-nodes-default --timeout 300s
+# end::await-cluster[]
 
 # wait a bit for the port to open
 sleep 10
 
-echo "Starting port-forwarding of port 8088"
+echo "Starting port-forwarding of port 9200"
 # tag::port-forwarding[]
-kubectl port-forward service/simple-superset-node 8088 > /dev/null 2>&1 &
+kubectl port-forward services/simple-opensearch 9200 > /dev/null 2>&1 &
 # end::port-forwarding[]
 PORT_FORWARD_PID=$!
 # shellcheck disable=2064 # we want the PID evaluated now, not at the time the trap is
 trap "kill $PORT_FORWARD_PID" EXIT
 sleep 5
 
-echo "Checking if web interface is reachable ..."
-return_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8088/login/)
-if [ "$return_code" == 200 ]; then
-  echo "Web interface reachable!"
-else
-  echo "Could not reach web interface."
-  exit 1
-fi
+echo "Using the REST API"
+# tag::rest-api[]
+export CREDENTIALS=admin:AJVFsGJBbpT6mChn
 
-echo "Loading examples ..."
-# tag::load-examples[]
-kubectl apply -f superset-load-examples-job.yaml
-sleep 5
-kubectl wait --for=condition=complete --timeout=1500s job/superset-load-examples
-# end::load-examples[]
+curl \
+    --insecure \
+    --user $CREDENTIALS \
+    --request PUT \
+    --json '{"name": "Stackable"}' \
+    https://localhost:9200/sample_index/_doc/1
+
+# Output:
+# {"_index":"sample_index","_id":"1","_version":1,"result":"created","_shards":{"total":2,"successful":1,"failed":0},"_seq_no":0,"_primary_term":1}
+
+curl \
+    --insecure \
+    --user $CREDENTIALS \
+    --request GET \
+    https://localhost:9200/sample_index/_doc/1
+
+# Output:
+# {"_index":"sample_index","_id":"1","_version":1,"_seq_no":0,"_primary_term":1,"found":true,"_source":{"name": "Stackable"}}
+# end::rest-api[]
