1406cbbd77f3ac3ba29834a82232a89a914d7640
Techassi <git@techassi.dev>
2025-11-06 12:50:52 +0100

    chore: Release 25.11.0-rc1 (#65)
    * chore: Release 25.11.0-rc1
    
    Signed-off-by: Techassi <git@techassi.dev>
    
    * fix: Unit test 'test_validate_ok' (#66)
    
    fix test_validate_ok unit test that was broken on release rc branch
    
    ---------
    
    Signed-off-by: Techassi <git@techassi.dev>
    Co-authored-by: Benedikt Labrenz <benedikt@labrenz.org>

--- Cargo.nix
+++ Cargo.nix
@@ -8189,7 +8189,7 @@ rec {
       };
       "stackable-opensearch-operator" = rec {
         crateName = "stackable-opensearch-operator";
-        version = "0.0.0-dev";
+        version = "25.11.0-rc1";
         edition = "2024";
         crateBin = [
           {
