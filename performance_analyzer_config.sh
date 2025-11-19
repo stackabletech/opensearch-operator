a1e1a1ca12a0408d2b22ecb146b4ba8b185d6c6d
Siegfried Weber <mail@siegfriedweber.net>
2025-11-19 16:19:17 +0000

    wip

---
+++ performance_analyzer_config.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+curl -k https://admin:AJVFsGJBbpT6mChn@localhost:9200/_plugins/_performanceanalyzer/config
---
+++ performance_analyzer_metrics.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+curl -v http://localhost:9600/_plugins/_performanceanalyzer/metrics/units
