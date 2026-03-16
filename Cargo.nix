b184e6fb39ea99cf6fb2332b0ab67c80dba62d5e
Nick Larsen <nick.larsen@stackable.tech>
2026-03-16 11:02:00 +0100

    chore: Release 26.3.0-rc1
    Signed-off-by: Nick Larsen <nick.larsen@stackable.tech>

--- Cargo.nix
+++ Cargo.nix
@@ -9633,7 +9633,7 @@ rec {
       };
       "stackable-opensearch-operator" = rec {
         crateName = "stackable-opensearch-operator";
-        version = "0.0.0-dev";
+        version = "26.3.0-rc1";
         edition = "2024";
         crateBin = [
           {
