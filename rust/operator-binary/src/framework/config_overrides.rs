3af18407a8ca71a32a5feea714875e835680230b
Siegfried Weber <mail@siegfriedweber.net>
2026-04-27 15:33:15 +0200

    chore: Regenerate charts

--- rust/operator-binary/src/framework/config_overrides.rs
+++ rust/operator-binary/src/framework/config_overrides.rs
@@ -4,7 +4,7 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use stackable_operator::{config::merge::Merge, schemars};
 
-/// Mergeable variant of [`stackable_operator::config_overrides::KeyValueConfigOverrides`]
+// Mergeable variant of [`stackable_operator::config_overrides::KeyValueConfigOverrides`]
 #[derive(Clone, Debug, Default, Deserialize, Eq, JsonSchema, Merge, PartialEq, Serialize)]
 pub struct KeyValueConfigOverrides {
     #[serde(flatten)]
